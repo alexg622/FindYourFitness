c627ee0a5124ebc2220f33bdb2f37f96eca95611
alexg622 <alex.gm62288@gmail.com>
2018-09-08 13:19:11 -0700

    Make nav bar title

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styles/Navbar.css'
+
+class Navbar extends React.Component{
+
+  render(){
+    return(
+      <div className='nav-container'>
+        <div className='nav-title'>Find Your Fitness</div>
+      </div>
+    )
+  }
+}
+
+export default Navbar
